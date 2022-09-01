295a3f6cfe64910326b1700271599a7b43300f80
Laceinstein <laceinstein@gmail.com>
2022-09-01 15:13:06 +0200

    ajout du provider dans main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import Provider from 'react-redux'
+import {Provider} from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
