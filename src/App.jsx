7313d3a65465ba607acf56b067b47629699d7309
Laceinstein <laceinstein@gmail.com>
2022-09-01 15:10:14 +0200

    ajout dans le composant APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchingPosts } from './redux/actions/actionCreators'
 
 function App() {
   
+  const posts = useSelector((store) => store.posts)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if(posts.length < 1){
+      dispatch(fetchingPosts())
+    }
+  }, [posts])
 
   return (
     <div className="App">
